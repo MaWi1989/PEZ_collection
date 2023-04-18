0d06f2727642c748243b112b36d152a7569317c4
Manja Wilson <manjafiebig@aol.com>
2023-04-18 01:40:52 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you'll never guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MIDIFICATIONS = False 
